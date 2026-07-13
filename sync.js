1c0df778b367e219a73b30c8a687ad81d798bcd6
DarioLorenzon <162430743+DarioLorenzon@users.noreply.github.com>
2026-07-13 08:35:10 +0200

    Create sync.js

---
+++ sync.js
@@ -0,0 +1,36 @@
+/* =====================================
+   GOLDIES
+   -------------------------------------
+
+   Modul:
+   sync.js
+
+   Version:
+   2.2.0a
+
+   Autor:
+   Dario Lorenzon
+
+   Beschreibung:
+   CSV ↔ Firestore Synchronisation
+
+   Diese Datei enthält künftig:
+
+   - CSV Analyse
+   - CSV Synchronisation
+   - Aufbau einer neuen Tabelle
+   - Vergleich CSV ↔ Firestore
+   - Zusammenfassung der Änderungen
+
+===================================== */
+
+
+/* =====================================
+   CSV ANALYSE
+===================================== */
+
+async function analyseCSV() {
+
+    alert("sync.js erfolgreich eingebunden.");
+
+}
